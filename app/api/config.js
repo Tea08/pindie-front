0f17a4a9b8014a6b8bdf684755d3e73292c1070d
Tea08 <tea.kaa@yandex.ru>
2024-05-22 15:01:50 +0500

    URL

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://api-code-2.practicum-team.ru";
-export const BASE_URL = "https://tea-front.nomoredomainswork.ru/api";
+export const BASE_URL = "https://tea.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
